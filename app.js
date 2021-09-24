d62e002638d2dde4a3cc8a7df5def1829a35686e
Олег Філяк <73895897+OlehFilyak@users.noreply.github.com>
2021-09-24 15:11:29 +0300

    add simple routing with express

--- app.js
+++ app.js
@@ -1,5 +1,20 @@
 const http = require('http');
 
-const server = http.createServer();
+const express = require('express');
 
-server.listen(3000);
+const app = express();
+
+app.use('/', (req, res, next) => {
+  console.log('This always runs!');
+  next();
+});
+
+app.use('/add-product', (req, res, next) => {
+  res.send('<h1>Add product page!</h1>');
+});
+
+app.use('/', (req, res, next) => {
+  res.send('<h1>Main page!</h1>');
+});
+
+app.listen(3000);
